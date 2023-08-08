24d79472fdc3e6b604a93b3b3a6956887f0a317e
Noob Evad <davefrankmiranda@gmail.com>
2023-08-08 15:49:56 -0500

    put and get should work now

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const item = {
+content: content
+  };
+  
+  await store.put(item);
+  console.log('data has been added to the db');
+  
+  
+
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const contactDb = await openDB('contact', 1);
+  const tx = contactDb.transaction('contact', 'readonly');
+  const store = tx.objectStore('contact');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  console.error('getDb not implemented');
+
+  return result;
+
+
+
+
+};
 
 initdb();
